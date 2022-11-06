5c26aefb82e1681c4b8a9d85c31a0edde5b74c27
João Ferreira <joaoadrianoferreira@gmail.com>
2022-11-06 14:11:19 +0000

    fix

--- index.js
+++ index.js
@@ -1,27 +1,55 @@
-const cors = require('cors')
-const express = require('express')
-const Apollo = require('apollo-server-express')
-const app = express()
-const port = 3000
+const cors = require('cors'); 
+const express = require('express'); 
+const Apollo = require('apollo-server-express'); 
+const app = express(); 
+const port = 3000; 
+
+const schema = Apollo.gql `
+    type Query {
+        me: User,
+        user(id: ID!): User,
+        users: [User!],
+        message(id: ID!): Message,
+        messages: [Message!]
+    }
+
+    type User {
+        id: ID!,
+        username: String!,
+        special: String!,
+        messages: [Message!]
+    }
+
+    type Message {
+        id: ID!,
+        text: String!,
+        user: User!
+    }
+
+    type Mutation {
+        createMessage(text: String!, userID: ID!): Message!,
+        deleteMessage(id:ID!): Boolean!
+    }
+`; 
 
 let users = {
     1: {
         id: 1, 
-        username: "João Ferreira",
+        username: "João Ferreira"
     },
     2: {
         id: 2, 
-        username: "Manuel Marques",
-    }, 
+        username: "Manuel Marques"
+    },
     3: {
         id: 3, 
-        username: "Pedro Couto",
+        username: "Pedro Costa"
     }
 }
 
 let messages = {
     1: {
-        id: 1, 
+        id: 1,
         text: "Hello",
         userID: 1
     },
@@ -37,43 +65,14 @@ let messages = {
     }
 }
 
-const schema = Apollo.gql `
-    type Query {
-        me: User
-        user(id: ID!): User
-        users: [User!]
-
-        message(id: ID!): Message
-        messages: [Message!]
-    }
-
-    type User {
-        id: ID!
-        username: String!
-        special: String!
-        messages: [Message!]
-    }
-
-    type Message {
-        id: ID!
-        text: String!
-        user: User!
-    }
-
-    type Mutation {
-        createMessage(text: String!, userID: ID!): Message!
-        deleteMessage(id: ID!): Boolean!
-    }
-`;
-
 const resolvers = {
     Query: {
-        me: () => {
-            return users[1]
+        me: ()=> {
+            return users[1];
         },
         user: (parent, {id}) => {
             return users[id]
-        }, 
+        },
         users: () => {
             return Object.values(users)
         },
@@ -89,32 +88,32 @@ const resolvers = {
         createMessage: (parent, {text, userID}) => {
             let id = Object.keys(messages).length + 1
             let message = {
-                id: id,
-                text: text,
+                id: id, 
+                text: text, 
                 userID: userID
             }
 
-            messages[id] = message; 
+            messages[id] = message
 
-            return message; 
+            return message
         },
         deleteMessage: (parent, {id}) => {
-            if (!messages[id]) {
-                return false;
+            if(!messages[id]) {
+                return false
             } else {
-                delete messages[id];
-                return true;
+                delete messages[id]
+                return true
             }
         }
     },
 
     User: {
         special: parent => {
-            return parent.username + "_" + parent.id
-        }, 
+            return parent.username + " - " + parent.id
+        },
         messages: user => {
             return Object.values(messages).filter(
-                message => message.userID == user.id
+                message => message.userID === user.id
             )
         }
     },
@@ -124,15 +123,17 @@ const resolvers = {
             return users[message.userID]
         }
     }
-}; 
+}
 
 const server = new Apollo.ApolloServer({
     typeDefs: schema, 
     resolvers
-}); 
+})
 
-server.applyMiddleware({app, path: '/graphql'}); 
-app.use(cors()); 
-app.listen(port, function() {
-    console.log("Apollo Server on localhost:" + port + "/graphql"); 
-})  
+server.start().then(()=> {
+    server.applyMiddleware({app, path: '/graphql'});
+    app.use(cors); 
+    app.listen(port, ()=>{
+        console.log('Apollo Server is running on localhos:' + port + '/graphql');
+    })
+})
