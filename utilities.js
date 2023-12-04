98b0b655f38e0ecef1ab69838c6a5a888dd8706f
João Ferreira <joaoadrianoferreira@gmail.com>
2023-12-04 11:54:59 +0000

    bug fix

--- utilities.js
+++ utilities.js
@@ -1,28 +1,34 @@
+const jwt = require("jsonwebtoken");
+const { errorType } = require("./error");
 
-const jwt = require('jsonwebtoken');
-const { errorType } = require('./error')
-
-const secret = "%)$2sF55Idf(Rm&jyPnkqAL^+8m4dSw)"; 
+const secret = "%)$2sF55Idf(Rm&jyPnkqAL^+8m4dSw)";
 
 const generateToken = (user_info) => {
-    let token = jwt.sign({
-        data: user_info,
-    }, secret, {expiresIn: '24h'});
-    return token; 
-}
+  let token = jwt.sign(
+    {
+      data: user_info,
+    },
+    secret,
+    { expiresIn: "24h" }
+  );
+  return token;
+};
 
 const validateToken = async (token) => {
-    if(!token) {
-        return null; 
-    }
-    let validation = await jwt.verify(token.replace('Bearer ', ''), secret)
-    return validation;
-}
+  if (!token) {
+    return null;
+  }
+  try {
+    return await jwt.verify(token.replace("Bearer ", ""), secret);
+  } catch (err) {
+    return null;
+  }
+};
 
-const getErrrorCode = errorName => {
-    return errorType[errorName]
-}
+const getErrrorCode = (errorName) => {
+  return errorType[errorName];
+};
 
-exports.getErrrorCode = getErrrorCode
-exports.generateToken = generateToken
-exports.validateToken = validateToken
+exports.getErrrorCode = getErrrorCode;
+exports.generateToken = generateToken;
+exports.validateToken = validateToken;
